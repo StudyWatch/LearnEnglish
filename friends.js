e5c9e5451ca857af0be25f3d1bed26fd55363f29
StudyWatch <timor34@gmail.com>
2024-06-14 15:00:52 +0300

    Update friends.js

--- friends.js
+++ friends.js
@@ -546,7 +546,7 @@ searchInputSeries.addEventListener("input", function() {
         return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     }
 
-    fetch('episodeLinks.json')
+    fetch('./episodeLinks.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
